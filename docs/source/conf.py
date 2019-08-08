b05abafc7b7d181086c75ddb16d77e443acda082
ToughBishop <rowan.swiers@gmail.com>
2019-08-08 16:21:31 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,8 @@ templates_path = ['_templates']
 exclude_patterns = ['**.ipynb_checkpoints','_build']
 
 
+master_doc = 'index' # to stop read the docs complaining
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
